21f22f24bf074fd21725b63020120d6209bdb673
Anurag Singh Bhadauria <76035419+Anuraagsinghbhadauria@users.noreply.github.com>
2021-10-20 12:17:15 +0530

    Update GetDayOfYear.java

--- GetDayOfYear.java
+++ GetDayOfYear.java
@@ -7,14 +7,14 @@ public class GetDayOfYear {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+        String s = sc.nextLine(); //taking string input
 
-        String[] c = s.split("-");
+        String[] c = s.split("-"); //converting into list
         int year = Integer.parseInt(c[0]);
         int month = Integer.parseInt(c[1]);
         int date = Integer.parseInt(c[2]);
 
-        LocalDate l = LocalDate.of(year, month, date);
-        System.out.println(l.getDayOfYear());
+        LocalDate l = LocalDate.of(year, month, date); 
+        System.out.println(l.getDayOfYear()); //printing the output
     }
 }
